7a7916f950a28db5fa084b9b74740fc5a6fa21b6
JS <alvaro.orozco.tech@gmail.com>
2021-06-21 20:56:15 -0400

    Added Question model

--- dietApi/models.py
+++ dietApi/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
     firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    occupation = models.CharField(max_length=200)
+    dateOfBirth = models.DateField()
+    height = models.CharField(max_length=10)
+    gender = models.CharField(max_length=50)
+    createdAt = models.DateTimeField()
+    updatedAt = models.DateTimeField()
+
+class Question:
+    question = models.CharField(max_length=200)
     createdAt = models.DateTimeField()
     updatedAt = models.DateTimeField()
